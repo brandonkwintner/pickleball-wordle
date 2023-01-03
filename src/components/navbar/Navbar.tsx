12e18e13b11dac1f00d9df84cce0aa1723b22401
brandonkwintner <brandon.kwintner@gmail.com>
2023-01-03 17:09:20 -0500

    background color changes and add logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import {
   InformationCircleIcon,
 } from '@heroicons/react/outline'
 
+import logo from '../../assets/pickleball-clinic-rect.jpeg'
 import { ENABLE_ARCHIVED_GAMES } from '../../constants/settings'
 import { GAME_TITLE } from '../../constants/strings'
 
@@ -22,28 +23,32 @@ export const Navbar = ({
   setIsSettingsModalOpen,
 }: Props) => {
   return (
-    <div className="navbar">
-      <div className="navbar-content px-5 short:h-auto">
+    <div className="navbar bg-white">
+      <div className="navbar-content px-5 short:h-[100px]">
         <div className="flex">
           <InformationCircleIcon
-            className="h-6 w-6 cursor-pointer dark:stroke-white"
+            className="h-12 w-6 cursor-pointer dark:stroke-black"
             onClick={() => setIsInfoModalOpen(true)}
           />
           {ENABLE_ARCHIVED_GAMES && (
             <CalendarIcon
-              className="ml-3 h-6 w-6 cursor-pointer dark:stroke-white"
+              className="ml-3 h-12 w-6 cursor-pointer dark:stroke-white"
               onClick={() => setIsDatePickerModalOpen(true)}
             />
           )}
         </div>
-        <p className="text-xl font-bold dark:text-white">{GAME_TITLE}</p>
+        <div>
+          <p className="text-xl font-bold dark:text-black">{GAME_TITLE}</p>
+          <img alt="pickleball-clinic" src={logo} className="mb-1 h-12 w-44" />
+        </div>
+
         <div className="right-icons">
           <ChartBarIcon
-            className="mr-3 h-6 w-6 cursor-pointer dark:stroke-white"
+            className="mr-3 h-12 w-6 cursor-pointer dark:stroke-black"
             onClick={() => setIsStatsModalOpen(true)}
           />
           <CogIcon
-            className="h-6 w-6 cursor-pointer dark:stroke-white"
+            className="h-12 w-6 cursor-pointer dark:stroke-black"
             onClick={() => setIsSettingsModalOpen(true)}
           />
         </div>
